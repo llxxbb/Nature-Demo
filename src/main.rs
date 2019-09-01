79aa98672a414e4454b32f3bbeeef8ed6de4f490
llxxbb <llxxbb@yeah.net>
2019-09-01 19:26:44 +0800

    create fetch method for Nature

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,52 @@
+#[macro_use]
+extern crate lazy_static;
+
+use reqwest::Client;
+
+pub use nature_demo_common::{Commodity, Order, SelectedCommodity};
+
+lazy_static! {
+    static ref CLIENT : Client = Client::new();
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Nature demo");
 }
 
 #[cfg(test)]
-mod use_case{
-    use nature_demo_common::{Order, SelectedCommodity, Commodity};
+mod demo {
+    use super::*;
+
+    static URL_INPUT: &str = "http://localhost:8080/input";
 
     #[test]
-    fn create_new_order(){
-        let order = Order{
+    fn create_new_order() {
+        // create an order
+        let order = create_order();
+        // send
+        let response = CLIENT.post(URL_INPUT).json(&order).send();
+        match response {
+            Err(e) => { dbg!(e); }
+            Ok(res) => { dbg!(res); }
+        };
+        // check created instance
+    }
+
+    fn create_order() -> Order {
+        Order {
+            user_id: 123,
             price: 100,
             items: vec![
-                SelectedCommodity{
+                SelectedCommodity {
                     item: Commodity { id: 1, name: "phone".to_string() },
-                    num: 1
-                }
-                SelectedCommodity{
+                    num: 1,
+                },
+                SelectedCommodity {
                     item: Commodity { id: 2, name: "battery".to_string() },
-                    num: 2
+                    num: 2,
                 }
-            ]
-        };
+            ],
+            address: "a.b.c".to_string(),
+        }
     }
 }
