41d118b4653cf39df634b3f5b7ece439cc1ab13c
llxxbb <llxxbb@yeah.net>
2019-11-17 19:55:33 +0800

    fix demo sql

--- doc/demo.sql
+++ doc/demo.sql
@@ -55,4 +55,4 @@ VALUES('/B/sale/orderState:1', '/B/third/waybill:1', '{"selector":{"source_state
 -- waybill --> orderState:dispatching
 INSERT INTO relation
 (from_meta, to_meta, settings)
-VALUES('/B/third/waybill:1', '/B/sale/orderState:1', '{"target_states":{"dispatching":["new"]}}');
+VALUES('/B/third/waybill:1', '/B/sale/orderState:1', '{"target_states":{"add":["dispatching"]}}');
