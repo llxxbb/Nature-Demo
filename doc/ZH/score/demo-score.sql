dd1756364213d229c29ce7cffcba10a40c63e54d
llxxb <llxxbb@yeah.net>
2020-05-01 19:27:34 +0800

    - builtin executor: timer

--- doc/ZH/score/demo-score.sql
+++ doc/ZH/score/demo-score.sql
@@ -22,11 +22,18 @@ INSERT INTO relation
 (from_meta, to_meta, settings)
 VALUES('B:score/trainee/subject:1', 'B:score/trainee/all-subject:1', '{"use_upstream_id":true,"target":{"upstream_para":[0,1]},"executor":{"protocol":"builtIn","url":"sum","settings":"{\\"para_part\\":2}"}}');
 
--- subject top 3 ---------------------------------------------
+-- generate timer  ---------------------------------------------
 
 INSERT INTO meta
 (meta_type, meta_key, description, version, states, fields, config)
-VALUES('B', 'score/subject/top-timer', 'a timer to trigger top calculate', 1, '', '', '');
+VALUES('B', 'score/subject/top-timer', 'a timer to trigger top calculate', 1, '', '', '{"cache_saved":true}');
+
+INSERT INTO relation
+(from_meta, to_meta, settings)
+VALUES('B:score/trainee/subject:1', 'B:score/subject/top-timer:1', '{"use_upstream_id":true,"{"executor":{"protocol":"builtIn","url":"timer","settings":"{\\"para_part\\":2}"}}');
+
+
+-- subject top 3 ---------------------------------------------
 
 INSERT INTO meta
 (meta_type, meta_key, description, version, states, fields, config)
