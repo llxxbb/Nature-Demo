f58afe5502dc39b7002a0a1e544e517c03c9fc9e
llxxb <llxxbb@yeah.net>
2020-07-29 08:39:19 +0800

    - fix bug for instance_dao::get_by_key_range - fix bug for task_dao::raw_to_error

--- doc/demo-sale-statistics.sql
+++ doc/demo-sale-statistics.sql
@@ -5,7 +5,6 @@ TRUNCATE TABLE `task`;
 TRUNCATE TABLE `task_error`;
 
 -- generate time range based on `order` ---------------------------------------------
-
 INSERT INTO meta
 (meta_type, meta_key, description, version, states, fields, config)
 VALUES('B', 'sale/order', 'order', 1, '', '', '');
@@ -14,17 +13,9 @@ INSERT INTO meta
 (meta_type, meta_key, description, version, states, fields, config)
 VALUES('B', 'sale/item/tag_second', 'time range for second' , 1, '', '', '{"cache_saved":true}');
 
-INSERT INTO meta
-(meta_type, meta_key, description, version, states, fields, config)
-VALUES('B', 'sale/item/tag_minute', 'minute time range for minute' , 1, '', '', '{"cache_saved":true}');
-
 INSERT INTO relation
 (from_meta, to_meta, settings)
 VALUES('B:sale/order:1', 'B:sale/item/tag_second:1', '{"executor":{"protocol":"builtIn","url":"time_range"}}');
-
-INSERT INTO relation
-(from_meta, to_meta, settings)
-VALUES('B:sale/item/tag_second:1', 'B:sale/item/tag_minute:1', '{"executor":{"protocol":"builtIn","url":"time_range","settings":"{\\"on_para\\":true,\\"unit\\":\\"m\\",\\"value\\":5}"}}');
 
 -- item statistics index ---------------------------------------------
 -- second data
@@ -34,7 +25,7 @@ VALUES('B', 'sale/item/counter/s', 'how many item sold in second' , 1, '', '', '
 
 INSERT INTO meta
 (meta_type, meta_key, description, version, states, fields, config)
-VALUES('B', 'sale/item/money/s', 'how much money received in second' , 1, '', '', '');
+VALUES('B', 'sale/item/money/s', 'how much money received in a second for one item' , 1, '', '', '');
 
 INSERT INTO meta
 (meta_type, meta_key, description, version, states, fields, config)
@@ -42,9 +33,13 @@ VALUES('M', 'sale/order/second', '', 1, '', '', '{"multi_meta":["B:sale/item/cou
 
 INSERT INTO relation
 (from_meta, to_meta, settings)
-VALUES('B:sale/item/tag_second:1', 'M:sale/order/second:1', '{"delay_on_para":[2,1],"executor":{"protocol":"http","url":"http://localhost:8082/order_to_item"}}');
+VALUES('B:sale/item/tag_second:1', 'M:sale/order/second:1', '{"filter_before":[{"protocol":"builtIn","url":"instance-loader","settings":"{\\"key_gt\\":\\"B:sale/order:1|\\",\\"key_lt\\":\\"B:sale/order:2|\\",\\"time_part\\":[0,1]}"}],"delay_on_para":[2,1],"executor":{"protocol":"http","url":"http://localhost:8082/order_to_item"}}');
 
 -- minute data
+INSERT INTO meta
+(meta_type, meta_key, description, version, states, fields, config)
+VALUES('B', 'sale/item/tag_minute', 'minute time range for minute' , 1, '', '', '{"cache_saved":true}');
+
 INSERT INTO meta
 (meta_type, meta_key, description, version, states, fields, config)
 VALUES('B', 'sale/item/counter/m', 'how many item sold in minute' , 1, '', '', '');
@@ -53,3 +48,6 @@ INSERT INTO meta
 (meta_type, meta_key, description, version, states, fields, config)
 VALUES('B', 'sale/item/money/m', 'how much money received in minute' , 1, '', '', '');
 
+INSERT INTO relation
+(from_meta, to_meta, settings)
+VALUES('B:sale/item/tag_second:1', 'B:sale/item/tag_minute:1', '{"executor":{"protocol":"builtIn","url":"time_range","settings":"{\\"on_para\\":true,\\"unit\\":\\"m\\",\\"value\\":5}"}}');
