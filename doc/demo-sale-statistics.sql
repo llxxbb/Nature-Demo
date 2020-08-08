81104e0e0ae486f1c60f59652585dea93690222f
llxxb <llxxbb@yeah.net>
2020-08-08 11:26:19 +0800

    - builtin-merge: add KeyType::None support

--- doc/demo-sale-statistics.sql
+++ doc/demo-sale-statistics.sql
@@ -37,30 +37,34 @@ VALUES('B', 'sale/item/count/tag_second', 'time range for second' , 1, '', '', '
 
 INSERT INTO relation
 (from_meta, to_meta, settings)
-VALUES('B:sale/item/money:1', 'B:sale/item/money/tag_second:1', '{"target":{"append_para":[0]},"executor":{"protocol":"builtIn","url":"time_range"}}');
+VALUES('B:sale/item/money:1', 'B:sale/item/money/tag_second:1', '{"target":{"append_para":[0],"context_name":"(item)"},"executor":{"protocol":"builtIn","url":"time_range"}}');
 
 INSERT INTO relation
 (from_meta, to_meta, settings)
-VALUES('B:sale/item/count:1', 'B:sale/item/count/tag_second:1', '{"target":{"append_para":[0]},"executor":{"protocol":"builtIn","url":"time_range"}}');
+VALUES('B:sale/item/count:1', 'B:sale/item/count/tag_second:1', '{"target":{"append_para":[0],"context_name":"(item)"},"executor":{"protocol":"builtIn","url":"time_range"}}');
 
--- item statistics ---------------------------------------------
-
-INSERT INTO meta
-(meta_type, meta_key, description, version, states, fields, config)
-VALUES('B', 'sale/item/money/second', 'time range for second' , 1, '', '', '{"cache_saved":true}');
+-- second statistics ---------------------------------------------
 
 INSERT INTO meta
 (meta_type, meta_key, description, version, states, fields, config)
-VALUES('B', 'sale/item/count/second', 'time range for second' , 1, '', '', '{"cache_saved":true}');
+VALUES('B', 'sale/item/money/second', 'second summary of money' , 1, '', '', '');
+
+INSERT INTO meta
+(meta_type, meta_key, description, version, states, fields, config)
+VALUES('B', 'sale/item/count/second', 'second summary of count' , 1, '', '', '');
 
 INSERT INTO relation
 (from_meta, to_meta, settings)
-VALUES('B:sale/item/money/tag_second:1', 'B:sale/item/money/second:1', '{"delay_on_para":[2,1],"executor":{"protocol":"builtIn","url":"time_range"}}');
+VALUES('B:sale/item/count/tag_second:1', 'B:sale/item/count/second:1', '{"filter_before":[{"protocol":"builtIn","url":"instance-loader","settings":"{\\"key_gt\\":\\"B:sale/item/count:1|0|(item)/\\",\\"key_lt\\":\\"B:sale/item/count:1|0|(item)0\\",\\"time_part\\":[0,1]}"}],"delay_on_para":[2,1],"executor":{"protocol":"builtIn","url":"merge"}}');
+
+INSERT INTO relation
+(from_meta, to_meta, settings)
+VALUES('B:sale/item/money/tag_second:1', 'B:sale/item/money/second:1', '{"filter_before":[{"protocol":"builtIn","url":"instance-loader","settings":"{\\"key_gt\\":\\"B:sale/item/money:1|0|(item)/\\",\\"key_lt\\":\\"B:sale/item/money:1|0|(item)0\\",\\"time_part\\":[0,1]}"}],"delay_on_para":[2,1],"executor":{"protocol":"builtIn","url":"merge"}}');
 
 
 -- item statistics ---------------------------------------------
 
--- "filter_before":[{"protocol":"builtIn","url":"instance-loader","settings":"{\\"key_gt\\":\\"B:sale/order:1|\\",\\"key_lt\\":\\"B:sale/order:2|\\",\\"time_part\\":[0,1],\\"filters\\":[{\\"protocol\\":\\"localRust\\",\\"url\\":\\"nature_demo_executor:order2item\\"}]}"}]
+-- "filter_before":[{"protocol":"builtIn","url":"instance-loader","settings":"{\\"key_gt\\":\\"B:sale/item/count:1|0|(item)/\\",\\"key_lt\\":\\"B:sale/item/count:1|0|(item)0\\",\\"time_part\\":[0,1],\\"filters\\":[{\\"protocol\\":\\"localRust\\",\\"url\\":\\"nature_demo_executor:make_tuple\\"}]}"}]
 
 -- minute data
 INSERT INTO meta
