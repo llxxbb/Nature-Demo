5661a258d5ae903297ed130d2684596bd55faed0
llxxb <llxxbb@yeah.net>
2020-08-26 09:08:34 +0800

    - optimize: normalize meta property for inputted instances

---
+++ doc/shell/email.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# This shell to simulate the business system client to communicate with Nature.
+
+## generate order-----------------------------------
+instance='{
+  "user_id":123,
+  "price":1000,
+  "items":[
+    {
+      "item":{
+        "id":1,
+        "name":"phone",
+        "price":800
+      },
+      num:1
+    },
+    {
+      "item":{
+        "id":2,
+        "name":"battery",
+        "price":100
+      },
+      num:1
+    }
+  ],
+  "address":"a.b.c"
+}'
+
+myMeta="B:/sale/order:1"
+
+## submit order to Nature
+
+JSON_STRING=$( jq -n \
+                  --arg meta "$myMeta" \
+                  --arg content "$instance" \
+                  '{"data":{"meta": $meta, "content": $content}}' )
+
+echo  $JSON_STRING
+
+url="http://localhost:8080/input"
+
+curl -H "Content-type: application/json" -X POST \
+     -d"$JSON_STRING" $url
+
