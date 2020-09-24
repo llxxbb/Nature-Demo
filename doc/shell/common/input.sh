27088fb6f483a8c4668b72cda6338d751fb9cfdd
llxxb <llxxbb@yeah.net>
2020-09-24 23:07:09 +0800

    - make ConverterReturned is fit for json translate

--- doc/shell/common/input.sh
+++ doc/shell/common/input.sh
@@ -9,6 +9,8 @@ JSON_STRING=$( jq -n \
                   --arg content "$2" \
                   '{"data":{"meta": $meta, "content": $content}}' )
 
+#echo "$JSON_STRING"
+
 # sed -e 's/^"//' -e 's/"$//' used to remove " at that surround with the value
 curl -H "Content-type: application/json" -X POST \
      -d"$JSON_STRING" http://localhost:8080/input | jq '.Ok' | sed -e 's/^"//' -e 's/"$//'
