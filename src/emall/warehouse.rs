accc19c9de3c25c5399717764059c8f1749c8383
llxxbb <llxxbb@yeah.net>
2021-01-30 20:05:40 +0800

    u64 -> String

--- src/emall/warehouse.rs
+++ src/emall/warehouse.rs
@@ -1,11 +1,12 @@
-use crate::{send_instance, wait_for_order_state};
 use nature::domain::Instance;
 
-pub fn outbound(order_id: u64) {
+use crate::{send_instance, wait_for_order_state};
+
+pub fn outbound(order_id: &str) {
     // for package
     let last = wait_for_order_state(order_id, 3);
     let mut instance = Instance::new("sale/orderState").unwrap();
-    instance.id = last.id;
+    instance.id = last.id.to_string();
     instance.state_version = last.state_version + 1;
     instance.states.clear();
     instance.states.insert("outbound".to_string());
